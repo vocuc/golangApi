6279129b3b08d85dbc7dbc8dd2950dc0cb5aaaee
Quân Bùi <buithequantk40l@gmail.com>
2020-08-24 11:25:08 +0700

    no message

---
+++ github.com/quanbt/shopeva/models/setup_example.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	_ "github.com/go-sql-driver/mysql" //...
+	"github.com/jinzhu/gorm"
+)
+
+//DB ...
+var DB *gorm.DB
+
+//ConnectDataBase ...
+func ConnectDataBase() {
+	database, err := gorm.Open("mysql", "root:123456@/shopeva?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	DB = database
+}
